ff5bc0fb5ffb9462c9e00ca38e213f687819c14f
NoAndernack <andernackn@gmail.com>
2023-12-13 14:50:50 +0100

    V3

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let australie = {
+    name:"Australie",
     climat : "Une grande partie du pays est donc aride, avec un climat sec. Dans le nord, le climat est tropical",
     continent : "L'océanie",
     capital : "La capital est Canberra",
@@ -7,6 +8,7 @@ let australie = {
     monnaie :"La monnaie en Australie est le dollar australien ( AUD )",
 }
 let usa = {
+    name:"Etats-unies",
     climat :"Le climat est globalement continental subarctique aux hivers glacés et aux étés frais ou froids. Dans l'extrême nord, le climat est polaire. Sur la côte sud-est, il est généralement océanique. Il faut aussi tenir compte de l'altitude et de la proximité de la mer.",
     continent : "Amérique du Nord",
     capital :"La capital est Washington",
@@ -14,6 +16,7 @@ let usa = {
     monnaie : "L'unité monétaire est le dollar américain ( USD )",
 }
 let newZealand = {
+    name:"Nouvelle-Zélande",
     climat :"La Nouvelle-Zélande a un climat de type tempéré océanique avec une tendance subtropicale au Nord.",
     continent : "L'océanie",
     capital : "La capital est Wellington",
@@ -21,6 +24,7 @@ let newZealand = {
     monnaie : "La monnaie de la Nouvelle Zelande est le dollar néo-zélandais ( NZD ou $NZ )",
 }
 let espagne = {
+    name:"Espagne",
     climat :"Le climat de l'Espagne est pour la plus grande partie du territoire un climat méditerranéen. Les températures moyennes annuelles y sont élevées et les précipitations relativement faibles voire insuffisantes en été.",
     continent :"Europe",
     capital : "La capital est Madrid",
@@ -28,6 +32,7 @@ let espagne = {
     monnaie : "L'euro est la monnaie en vigueur dans le pays( EUR )",
 }
 let portugal = {
+    name:"Portugal",
     climat :"Le climat du Portugal est de type méditerranéen selon la classification de Köppen. D'après cette classification, le climat est caractérisé essentiellement par des étés chauds et secs et des hivers plus ou moins doux.",
     continent : "Europe",
     capital : "La capital est Lisbonne",
@@ -35,6 +40,7 @@ let portugal = {
     monnaie : "L'Euro est la monnaie en cours au Portugal depuis l'année 2002.( EUR )",
 }
 let madagascare = {
+    name:"Madagascare",
     climat :"Le climat à Madagascar est assez humide sur la côte Est. Le pic d'humidité a lieu de décembre à avril même si les précipitations peuvent survenir à d'autres moments de l'année, en dehors de la saison des pluies.",
     continent : "Afrique",
     capital : "La capital est Tananarive",
@@ -42,6 +48,7 @@ let madagascare = {
     monnaie : "L'ariary  est une monnaie traditionnelle de la République de Madagascar( MGA )",
 }
 let grece = {
+    name:"Grèce",
     climat :"La Grèce jouit d'un climat méditerrannéen dans l'ensemble, qui varie cependant selon les régions, avec des hivers doux et pluvieux et des étés très chauds et secs, rafraîchis cependant par les vents du nord.",
     continent :"Europe",
     capital : "La capital est Athènes",
@@ -49,6 +56,7 @@ let grece = {
     monnaie :"La monnaie grecque est l'Euro.( EUR)",
 }
 let perou = {
+    name:"Perou",
     climat :"Le Pérou, vaste pays, a plusieurs climats : Lima et les côtes, climat tempéré : préférez les mois de décembre à avril. Les Andes et le Machu Picchu, climat plus froid : la saison sèche entre mai et octobre est la plus favorable. La région amazonienne, climat tropical : les mois conseillés sont juillet et août.",
     continent : "Amérique du Sud",
     capital : "La capital est Lima",
@@ -56,6 +64,7 @@ let perou = {
     monnaie :"La devise péruvienne est le Nouveau Sol, ou le Nuevo Sol.( PEN )",
 }
 let laos = {
+    name: "Laos",
     climat :"Le climat au Laos est de type tropical humide, avec deux grandes saisons. Il fait chaud toute l'année, avec une température moyenne de 29°C en janvier et de 35°C en avril. La saison sèche, de novembre à mars, est parfaite pour partir au Laos.",
     continent : "Asie",
     capital : "La capital est Vientiane",
@@ -63,6 +72,7 @@ let laos = {
     monnaie : "La monnaie nationale de la République démocratique populaire du Laos est le kip.( LAK )",
 }
 let philippine = {
+    name:"Philippines",
     climat :"Situé près de l'équateur, l'archipel des Philippines est soumis à un climat tropical. Il y fait chaud et humide toute l'année. On distingue deux types de moussons : Amihan est la mousson du nord-est, caractérisée par des vents frais et secs.",
     continent : "Asie",
     capital : "La capital est Manille",
@@ -71,3 +81,87 @@ let philippine = {
 }
 let carte = [australie , usa , newZealand , espagne , portugal , madagascare , grece  
     , perou , laos , philippine ]
+    
+
+    function cartesGenerateur() {
+        let main = document.querySelector("main");
+    
+        carte.forEach(element => {
+            // -------------CREATION DES BALISES-------------
+            let section = document.createElement("section");
+            let titre = document.createElement("h1");
+            let blocClimat = document.createElement("article");
+            let titreClimat = document.createElement("h2");
+            let paraClimat = document.createElement("p");
+            let blocContinent = document.createElement("article");
+            let titreContinent = document.createElement("h2");
+            let paraContinent = document.createElement("p");
+            let blocCapital = document.createElement("article");
+            let titreCapital = document.createElement("h2");
+            let paraCapital = document.createElement("p");
+            let blocPopulation = document.createElement("article");
+            let titrePopulation = document.createElement("h2");
+            let paraPopulation = document.createElement("p");
+            let blocMonnaie = document.createElement("article");
+            let titreMonnaie = document.createElement("h2");
+            let paraMonnaie = document.createElement("p");
+    
+            // -------------CREATION DES CLASSES-------------
+            section.classList.add("carte", `carte--${element.name}`);
+            titre.classList.add("carte__pays");
+            section.classList.add("carte");
+            blocClimat.classList.add("carte__climat")
+            blocContinent.classList.add("carte__continent")
+            blocCapital.classList.add("carte__capital")
+            blocPopulation.classList.add("carte__population")
+            blocMonnaie.classList.add("carte__monnaie")
+
+    
+            // ------------Ajoute les éléments créés à la section-----------------
+            section.appendChild(titre);
+            section.appendChild(blocClimat);
+            section.appendChild(blocContinent);
+            section.appendChild(blocCapital);
+            section.appendChild(blocPopulation);
+            section.appendChild(blocMonnaie);
+    
+            // -----------------Ajoute la section au "main"-------------------
+            main.appendChild(section);
+    
+            // -----------------Remplissage des éléments avec les données-------------------
+            
+            titre.textContent = element.name;
+            titreClimat.textContent = "Climat";
+            paraClimat.textContent = element.climat;
+    
+            titreContinent.textContent = "Continent";
+            paraContinent.textContent = element.continent;
+    
+            titreCapital.textContent = "Capital";
+            paraCapital.textContent = element.capital;
+    
+            titrePopulation.textContent = "Population";
+            paraPopulation.textContent = element.population;
+    
+            titreMonnaie.textContent = "Monnaie";
+            paraMonnaie.textContent = element.monnaie;
+    
+            // ------------Ajoute les éléments remplis à leurs blocs correspondants-----------------
+            blocClimat.appendChild(titreClimat);
+            blocClimat.appendChild(paraClimat);
+    
+            blocContinent.appendChild(titreContinent);
+            blocContinent.appendChild(paraContinent);
+    
+            blocCapital.appendChild(titreCapital);
+            blocCapital.appendChild(paraCapital);
+    
+            blocPopulation.appendChild(titrePopulation);
+            blocPopulation.appendChild(paraPopulation);
+    
+            blocMonnaie.appendChild(titreMonnaie);
+            blocMonnaie.appendChild(paraMonnaie);
+        });
+    }
+    
+    cartesGenerateur();
